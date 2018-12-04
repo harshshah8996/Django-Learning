66d89f0212ad5d685a0061fe3ca31ede88c37d15
harsh shah <harsh_shah@gridle.io>
2018-12-04 18:53:03 +0530

    install rest_framework and update requirements.txt

--- src/Employee_Management/settings.py
+++ src/Employee_Management/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'employee',
 ]
 
 MIDDLEWARE = [
@@ -90,6 +92,15 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':'3306'
     }
+}
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 # Password validation
