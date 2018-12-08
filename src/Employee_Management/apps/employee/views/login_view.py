25af73ba50f821cee1cd45071f796f4ab42fb823
harsh shah <harsh_shah@gridle.io>
2018-12-08 19:59:32 +0530

    generate token manually

--- src/Employee_Management/apps/employee/views/login_view.py
+++ src/Employee_Management/apps/employee/views/login_view.py
@@ -2,9 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_jwt.settings import api_settings
 from django.contrib.auth import authenticate
 
+from ..models import UserModel
+from ..serializers import UserSerializer
+
 import requests,json
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
 
 class LoginView(APIView):
 
@@ -21,9 +29,17 @@ class LoginView(APIView):
                 'password':password
             }
             
-            jwt_response = requests.post('http://localhost:7000/api-token-auth/',data=payload)
-            response = json.loads(jwt_response.text)
-            return Response({'data':response['token'],'message':('Welcome ' + str(user_obj.first_name))},status=status.HTTP_200_OK)
+            # default JWT implementation
+            # jwt_response = requests.post('http://localhost:7000/api-token-auth/',data=payload)
+            # response = json.loads(jwt_response.text)
+            # token = response['token']
+
+            # manually JWT generation
+
+            payload = jwt_payload_handler(user_obj)
+            token = jwt_encode_handler(payload)
+
+            return Response({'data':token,'message':('Welcome ' + str(user_obj.first_name))},status=status.HTTP_200_OK)
         
         else:
             return Response({'message':'Please Enter Valid Email & Password'},status=status.HTTP_406_NOT_ACCEPTABLE)
